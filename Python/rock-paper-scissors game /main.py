2d3eca87fbc2c719817883952cd4c0479aad93f4
Amritpal Singh <124516230+amritpalsingh52@users.noreply.github.com>
2023-10-03 19:05:29 +0530

    Added a project of rock_paper-scissors

---
+++ Python/rock-paper-scissors game /main.py
@@ -0,0 +1,40 @@
+import random
+
+def get_user_choice():
+    while True:
+        print("Enter your choice: rock, paper, or scissors")
+        user_choice = input().lower()
+        if user_choice in ['rock', 'paper', 'scissors']:
+            return user_choice
+        else:
+            print("Invalid choice. Please try again.")
+
+def get_computer_choice():
+    return random.choice(['rock', 'paper', 'scissors'])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Welcome to Rock-Paper-Scissors game!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        print(determine_winner(user_choice, computer_choice))
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing! Have a great day!")
+            break
+
+if __name__ == "__main__":
+    play_game()
